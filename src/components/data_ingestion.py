19b73af893a470a16cc8ebdc1cdcbac0abe5926c
sourabh22 <prajapatisourabh22@gmail.com>
2023-03-15 16:52:40 +0530

    eda & train & data ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -44,6 +44,6 @@ class DataIngestion:
         except Exception as e:
             raise CustomException(e, sys)
 
-if __name__=='__main__':
-    obj=DataIngestion()
-    obj.initiate_data_ingestion()
+# if __name__=='__main__':
+#     obj=DataIngestion()
+#     obj.initiate_data_ingestion()
